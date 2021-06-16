8a7f61968fce7ce527f2bc8df8c75eed112dbf3b
craminhos <craminhos@itclinical.com>
2021-06-16 20:19:09 +0100

    Hide modal and backdrop when the Cancel and Confirm button are clicked in modal

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,23 +1,31 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Modal from './Modal';
-import Backdrop from './Backdrop';
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
 
 function Todo(props) {
-  const [ modalIsOpen, setModalIsOpen ] = useState(false);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   function deleteHandler() {
     setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
   }
 
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button className="btn" onClick={deleteHandler}>Delete</button>
+        <button className="btn" onClick={deleteHandler}>
+          Delete
+        </button>
       </div>
-      {modalIsOpen && <Modal />}
-      {modalIsOpen && <Backdrop />}
+      {modalIsOpen && (
+        <Modal onConfirm={closeModalHandler} onCancel={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
