fba246aa9005bb7e4c12070fcba355efa0f0797e
craminhos <craminhos@itclinical.com>
2021-06-16 20:05:26 +0100

    Show modal and backdrop only when delete button is clicked in Todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,5 +1,13 @@
+import { useState } from 'react';
+
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
 function Todo(props) {
+  const [ modalIsOpen, setModalIsOpen ] = useState(false);
+
   function deleteHandler() {
+    setModalIsOpen(true);
   }
 
   return (
@@ -8,6 +16,8 @@ function Todo(props) {
       <div className="actions">
         <button className="btn" onClick={deleteHandler}>Delete</button>
       </div>
+      {modalIsOpen && <Modal />}
+      {modalIsOpen && <Backdrop />}
     </div>
   );
 }
