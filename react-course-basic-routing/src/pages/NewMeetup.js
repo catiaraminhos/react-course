d91e064696482935d251d972b2347906b4a62448
craminhos <craminhos@itclinical.com>
2021-06-23 19:24:32 +0100

    Prepare to send request to add a meetup

--- react-course-basic-routing/src/pages/NewMeetup.js
+++ react-course-basic-routing/src/pages/NewMeetup.js
@@ -1,9 +1,13 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    console.log(meetupData);
+  }
+
   return <section>
     <h1>Add New Meetup</h1>
-    <NewMeetupForm />
+    <NewMeetupForm onAddMeetup={addMeetupHandler} />
   </section>;
 }
 
