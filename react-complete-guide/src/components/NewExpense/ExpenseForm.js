ca4d036580b18b9f53817e7e8fe7e75282ab4b88
craminhos <craminhos@itclinical.com>
2021-07-06 19:37:55 +0100

    Clear inputs on expense form submission

--- react-complete-guide/src/components/NewExpense/ExpenseForm.js
+++ react-complete-guide/src/components/NewExpense/ExpenseForm.js
@@ -29,6 +29,10 @@ const ExpenseForm = () => {
         };
 
         console.log('expense data', expenseData);
+
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
 
     return (
@@ -36,7 +40,7 @@ const ExpenseForm = () => {
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
                 <div className="new-expense__control">
                     <label>Amount</label>
@@ -44,6 +48,7 @@ const ExpenseForm = () => {
                         type="number"
                         min="0.01"
                         step="0.01"
+                        value={enteredAmount}
                         onChange={amountChangeHandler}
                     />
                 </div>
@@ -53,6 +58,7 @@ const ExpenseForm = () => {
                         type="date"
                         min="2019-01-01"
                         step="2022-12-31"
+                        value={enteredDate}
                         onChange={dateChangeHandler}
                     />
                 </div>
