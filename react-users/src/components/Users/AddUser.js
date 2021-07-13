4b6a532015220af9510491274526dafcea66924b
craminhos <craminhos@itclinical.com>
2021-07-13 21:57:03 +0100

    Add AddUser component

---
+++ react-users/src/components/Users/AddUser.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './AddUser.module.css';
+
+function AddUser() {
+    return <div className={styles['add-user']}></div>;
+}
+
+export default AddUser;
