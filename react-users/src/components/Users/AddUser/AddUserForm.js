e5bc039e7b5d33d3f17a39ab6a886068af4ddf56
craminhos <craminhos@itclinical.com>
2021-07-13 22:22:31 +0100

    Add reusable Button component and use it on AddUserForm

--- react-users/src/components/Users/AddUser/AddUserForm.js
+++ react-users/src/components/Users/AddUser/AddUserForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '../../UI/Button';
 
 import styles from './AddUserForm.module.css';
 
@@ -9,10 +10,13 @@ const AddUserForm = () => {
                 <label>Username</label>
                 <input type="text" name="username" />
             </div>
+
             <div className={styles.input}>
                 <label>Age (Years)</label>
                 <input type="number" name="years" />
             </div>
+
+            <Button type='submit'>Add User</Button>
         </form>
     );
 };
